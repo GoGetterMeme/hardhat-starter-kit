7c7c4896c93f186bdb0bfcb09b64c12ef4388c8c
GoGetterMeme <willpurcaru@yahoo.com>
2021-06-29 16:47:23 -0400

    init commit

---
+++ contracts/QR_code.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract QR_code is AggregatorV3Interface {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Kovan
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    }
+    
+    /**
+     * Sends event on receive
+     */
+    event Received(address, uint);
+    
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        // If the round is not complete yet, timestamp is 0
+        require(timeStamp > 0, "Round not complete");
+        return price;
+    }
+}
